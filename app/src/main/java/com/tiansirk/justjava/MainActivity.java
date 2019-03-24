b2215c0862a65bfde888b673b05ce142cc0c7571
Tóth Krisztián <BF8EZM@tr.pte.hu>
2019-03-24 19:00:09 +0100

    Initiate project Just Java and Create layout

---
+++ app/src/main/java/com/tiansirk/justjava/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tiansirk.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
